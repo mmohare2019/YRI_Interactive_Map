67c9fa62d6f2f42fc71ef059d697368e2adfd059
mmohare2019 <55029888+mmohare2019@users.noreply.github.com>
2023-02-21 20:54:30 -0500

    express server with mongo db connection

--- App.js
+++
@@ -1,9 +0,0 @@
-// server application main file
-
-const express = require("express")
-const path = require('path');
-const app = express()
-
-app.use(express.static(path.join(__dirname, 'react-app/build')));
-
-app.listen(3000)
