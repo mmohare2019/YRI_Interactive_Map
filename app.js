92ef07791500bbb0bdfd4fdcc80746b53cc8e7e9
Joe Luhrman <jaluhrman2018@gmail.com>
2023-02-24 13:20:23 -0500

    updated directory strucutre and removed stuff

---
+++ app.js
@@ -0,0 +1,28 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var adminRouter = require("./routes/admin")
+
+var app = express();
+
+// view engine setup
+//app.set('views', path.join(__dirname, 'views'));
+app.set(express.static(path.join(__dirname, "react-app", "build")))
+app.set('view engine', 'jade'); // ?????????????
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use("/admin", adminRouter)
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+module.exports = app;
