e8ffc782bf99010e6e4bd59f816438401f135c55
mmohare2019 <55029888+mmohare2019@users.noreply.github.com>
2023-03-01 15:27:03 -0500

    finishing up message dao tests

--- models/messageDao.test.js
+++ models/messageDao.test.js
@@ -5,6 +5,14 @@ beforeAll(function() {
     dbcon.connect(); 
 });
 
+afterAll(async function () {
+    await dao.deleteAll();
+});
+
+beforeAll(async function () {
+    await dao.deleteAll(); 
+});
+
 test('Create new message', async function () {
     let newMessage = {
         title: "Address update",
@@ -25,4 +33,30 @@ test('Inbox of all messages', async function () {
     let created = await dao.create(newMessage);
     let inbox = await dao.inbox();
     expect(inbox).not.toBe(null);
-})
+});
+
+test("Delete a message by its id", async function () {
+    let newMessage = {
+        title: "Address update",
+        description: "I think this location needs to be updated"
+    };
+
+    let created = await dao.create(newMessage);
+    let inbox = await dao.inbox();
+    expect(inbox).not.toBe(null);
+
+    let _id = created._id;
+    await dao.deleteMessage(_id);
+    let msg = await dao.findMessage(_id);
+    expect(msg).toBe(null);
+});
+
+
+test("Delete all messages", async function() {
+    let inbox = await dao.inbox();
+    expect(inbox).not.toBe(null);
+
+    await dao.deleteAll();
+    inbox = await dao.inbox();
+    expect(inbox).toEqual([]);
+});
