83a9676dbd519b5cb76535a13a7ff4af1c38649c
mmohare2019 <55029888+mmohare2019@users.noreply.github.com>
2023-03-02 20:06:45 -0500

    snapshot testing

--- react-app/src/view/components/Header.test.js
+++ react-app/src/view/components/Header.test.js
@@ -1,9 +1,16 @@
 import TestRenderer from "react-test-renderer";
 import Header from "./Header";
 import React from "react";
+import renderer from 'react-test-renderer';
 
-describe("Footer render tests", () => {
+describe("Header render tests", () => {
     test("render smoke test", () => {
         const testRenderer = TestRenderer.create(<Header/>)
     })
-})
+    
+});
+
+test("header test", function () {
+    const component = renderer.create(<Header/>).toJSON();
+    expect(component).toMatchSnapshot(); 
+});
