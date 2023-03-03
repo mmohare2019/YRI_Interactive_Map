a53ffb7b967847bfc8900001e28543d9baa695bb
Joe Luhrman <jaluhrman2018@gmail.com>
2023-03-03 17:52:08 -0500

    for real this time

--- react-app/babel.config.js
+++ react-app/babel.config.js
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
 module.exports = {
     presets:[
         "@babel/preset-env",
         "@babel/preset-react"
     ]
-=======
-{
-    "presets": ["@babel/preset-env"]
->>>>>>> 0db03e8a91258b0cd8dcc1d9e217b31ee7c143e8
 }
