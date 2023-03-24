2186bf42a8298647980eeeb9871742bf9907b03b
Joe Luhrman <jaluhrman2018@gmail.com>
2023-03-24 16:47:20 -0400

    U4 getting partner data

--- react-app/src/view/components/Map.js
+++ react-app/src/view/components/Map.js
@@ -1,7 +1,27 @@
 import { MapContainer, TileLayer } from "react-leaflet"
 import 'leaflet/dist/leaflet.css'
+import React from "react"
+import axios from "axios"
 
 export default function Map() {
+    const [partners, setPartners] = React.useState(null)
+
+    const getPartners = async() => {
+        axios.get("/partner")
+        .then((response) => {
+            setPartners(response.data)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    }
+
+    if(partners === null) {
+        getPartners()
+    }
+
+    console.log(partners)
+
     return(
         <MapContainer center={[39.355, -76.609]} zoom={15} >
             <TileLayer
