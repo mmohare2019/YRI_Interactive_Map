ae4cfabd79105448716a854e5704f540fbc2d41b
mmohare2019 <55029888+mmohare2019@users.noreply.github.com>
2023-04-05 14:28:07 -0400

    reformatting the admin home page

--- react-app/src/view/screen/AdminHome.js
+++ react-app/src/view/screen/AdminHome.js
@@ -1,9 +1,30 @@
 import React from "react"
 import Header from "../components/Header";
+import Button from 'react-bootstrap/Button';
 
 const adminHandler = require("../../event-handler/adminHandler")
 
 export default class AdminHome extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.onSubmitMessages = this.onSubmitMessages.bind(this);
+        this.onSubmitPartners = this.onSubmitPartners.bind(this);
+        this.onSubmitCategories = this.onSubmitCategories.bind(this);
+    }
+
+    async onSubmitMessages() {
+        window.location.replace("/inbox");
+    }
+
+    async onSubmitPartners () {
+        window.location.replace("/partners");
+    }
+
+    async onSubmitCategories() {
+        window.location.replace("/category");
+    }
+
     render() {  
         return (
             <div>
@@ -13,22 +34,21 @@ export default class AdminHome extends React.Component {
                     York Road Initiative Interactive Map Admin Home
                 </h1>
 
-                <div>
-                    <ul>
-                        <li>
-                            <a href="http://localhost:3000/inbox">Manage messages</a>
-                        </li>
-                        <li>  
-                            <a href="http://localhost:3000/partners">Manage community partners</a>
-                        </li>
-                        <li>   
-                            <a href="http://localhost:3000/category">Manage community partner categories</a>
-                        </li>
-
-                        <button onClick={adminHandler.logout}>
+                <div className="container">
+                    <div className="d-grid gap-2">
+                        <Button variant="light" size="lg" onClick={this.onSubmitMessages}>
+                            Manage messages
+                        </Button>
+                        <Button variant="light" size="lg" onClick={this.onSubmitPartners}>
+                            Manage community partners 
+                        </Button>
+                        <Button variant="light" size="lg" onClick={this.onSubmitCategories}>
+                            Manage partner location categories
+                        </Button>
+                        <Button variant="secondary" size="lg" onClick={adminHandler.logout}>
                             Logout
-                        </button>
-                    </ul>
+                        </Button>
+                    </div>
                 </div>
 
             </div>
