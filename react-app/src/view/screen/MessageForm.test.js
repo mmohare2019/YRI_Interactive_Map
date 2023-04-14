014446fbf3e3cc3bff705666e21a7bb878ae6b63
mmohare2019 <55029888+mmohare2019@users.noreply.github.com>
2023-04-13 21:43:50 -0400

    finally figured out some jest testing!!

--- react-app/src/view/screen/MessageForm.test.js
+++ react-app/src/view/screen/MessageForm.test.js
@@ -1,8 +1,31 @@
 import React from "react"
+import { fireEvent, render, screen} from "@testing-library/react"
 import MessageForm from "./MessageForm"
-import {render} from "@testing-library/react"
+const messageHandler = require("../../event-handler/messageHandler")
 
 test("message form render smoke test", () => {
-    render(<MessageForm/>)
+    render(<MessageForm/>);
+
+    expect(screen.getByRole("heading", {name: "Submit a Request"})).toBeInTheDocument();
+    expect(screen.getByRole("button", {name: "Submit"})).toBeInTheDocument();
+
+    //const titleInput = result.container.querySelector("#title");
+    const titleInput = screen.getByPlaceholderText("Enter title");
+    fireEvent.change(titleInput, {target: {value: "test title"}});
+
+    //const descriptionInput = result.container.querySelector("#description");
+    const descriptionInput = screen.getByPlaceholderText("Description");
+    fireEvent.change(descriptionInput, {target: {value: "test description"}});
+
+    //const submitInput = result.container.querySelector("#submit")
+    const submitInput = screen.getByTestId("submit")
+
+    messageHandler.submitMessage = async(title, description) => {
+        return {
+            status: 200
+        }
+    } 
         
+    fireEvent.click(submitInput)
+
 });
